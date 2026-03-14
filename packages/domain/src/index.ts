acc89cf8b418f9826114e4cce751295af22adf12
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-03-14 00:30:34 -0300

    refactor: Delete domain index.ts, removing exports for Group, DeliveryItem, and helloDomain.

--- packages/domain/src/index.ts
+++
@@ -1,6 +0,0 @@
-export { Group } from "./Group.js";
-export { default as DeliveryItem } from "./DeliveryItem.js";
-
-export const helloDomain = () => {
-  return "Hello from the domain library!";
-};
