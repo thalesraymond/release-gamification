c2b8aa0c7b38df4007f322fd12d58b62316b5f11
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2026-03-14 00:43:51 -0300

    feat: Add `name` property to the `ReleaseCalendar` class.

--- packages/domain/src/ReleaseCalendar.ts
+++ packages/domain/src/ReleaseCalendar.ts
@@ -3,6 +3,7 @@ import MobileRelease from "./MobileRelease.js";
 export default class ReleaseCalendar {
   constructor(
     public readonly id: string,
+    public readonly name: string,
     public readonly mobileReleases: MobileRelease[],
   ) {}
 }
